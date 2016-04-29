f8114a33f1b565b62c448afed72251673066be66
HideyaChikiri <hideya2013.uma@gmail.com>
2016-04-29 06:48:04 +0000

    fnish graptest.py

---
+++ perceptron/graphtest.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+# SHHリモートログイン時はこれを利用
+import matplotlib
+matplotlib.use("Agg")
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+X = np.linspace(-np.pi, np.pi, 256, endpoint=True)
+C, S = np.cos(X), np.sin(X)
+
+# 関数のプロット
+plt.plot(X, C, color="blue", linewidth=3.0, linestyle="-", label="cos")
+plt.plot(X, S, color="red", linewidth=1.0, linestyle="-", label="sin")
+plt.legend(loc='upper left')
+
+# 注釈つける
+t = 2 * np.pi / 3
+plt.plot([t, t], [0, np.cos(t)], color='blue', linewidth=2.5, linestyle="--")
+plt.scatter([t, ], [np.cos(t), ], 50, color='blue')
+
+plt.plot([t, t],[0, np.sin(t)], color='red', linewidth=2.5, linestyle="--")
+plt.scatter([t, ],[np.sin(t), ], 50, color='red')
+
+plt.annotate(r'$cos(\frac{2\pi}{3})=-\frac{1}{2}$',
+             xy=(t, np.cos(t)), xycoords='data',
+             xytext=(-90, -50), textcoords='offset points', fontsize=16,
+             arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=.2"))
+
+plt.annotate(r'$sin(\frac{2\pi}{3})=\frac{\sqrt{3}}{2}$',
+             xy=(t, np.sin(t)), xycoords='data',
+             xytext=(+10, +30), textcoords='offset points', fontsize=16,
+             arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=.2"))
+
+# 見やすいようにスペースとる
+plt.xlim(X.min() * 1.1, X.max() * 1.1)
+plt.ylim(C.min() * 1.1, C.max() * 1.1)
+
+# メモリの設定
+plt.xticks([-np.pi, -np.pi/2, 0, np.pi/2, np.pi])
+plt.yticks([-1, 0, +1])
+
+# xy軸(spine)の移動
+ax = plt.gca()  # gca stands for 'get current axis'
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')
+ax.xaxis.set_ticks_position('bottom')
+ax.spines['bottom'].set_position(('data',0))
+ax.yaxis.set_ticks_position('left')
+ax.spines['left'].set_position(('data',0))
+
+# グラフとメモリの被りを半透明化
+for label in ax.get_xticklabels() + ax.get_yticklabels():
+    label.set_fontsize(16)
+    label.set_bbox(dict(facecolor='white', edgecolor='None', alpha=0.65))
+
+plt.savefig("graph.png") #CUIでshowは使えない
--- perceptron/perceptron.py
+++
@@ -1,3 +0,0 @@
-# coding: utf-8
-
-print("Hello World") #日本語もOK
