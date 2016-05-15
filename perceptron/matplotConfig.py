a59b1cf6e74cf1d5935be754f045d089ecc7ae86
HideyaChikiri <hideya2013.uma@gmail.com>
2016-05-15 04:15:26 +0000

    divide matplotConfig

---
+++ perceptron/matplotConfig.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+import matplotlib.pyplot as plt
+
+def forWeight(filename):
+    # xy軸(spine)の移動
+    ax = plt.gca()  # gca stands for 'get current axis'
+    ax.spines['right'].set_color('none')
+    ax.spines['top'].set_color('none')
+    ax.xaxis.set_ticks_position('bottom')
+    ax.spines['bottom'].set_position(('data',0))
+    ax.yaxis.set_ticks_position('left')
+    ax.spines['left'].set_position(('data',0))
+    ax.set_title(filename)
+    ax.set_xlabel('w1')
+    ax.set_ylabel('w0')
+    ax.grid(True) #補助線
+    
+    # グラフとメモリの被りを半透明化
+    for label in ax.get_xticklabels() + ax.get_yticklabels():
+        label.set_fontsize(16)
+        label.set_bbox(dict(facecolor='white', edgecolor='None', alpha=0.65))
+
+def forFeature(filename):
+    # xy軸(spine)の移動
+    ax = plt.gca()  # gca stands for 'get current axis'
+    ax.spines['right'].set_color('none')
+    ax.spines['top'].set_color('none')
+    ax.xaxis.set_ticks_position('bottom')
+    ax.spines['bottom'].set_position(('data',0))
+    ax.yaxis.set_ticks_position('left')
+    ax.spines['left'].set_position(('data',0))
+    ax.set_title(filename)
+    ax.set_xlabel('X')
+    ax.set_ylabel('g(X)')
+    ax.grid(True) #補助線
+    
+    # グラフとメモリの被りを半透明化
+    for label in ax.get_xticklabels() + ax.get_yticklabels():
+        label.set_fontsize(16)
+        label.set_bbox(dict(facecolor='white', edgecolor='None', alpha=0.65))
+        
--- perceptron/perceptron.py
+++ perceptron/perceptron.py
@@ -6,40 +6,18 @@ matplotlib.use("Agg")
 
 import numpy as np
 import matplotlib.pyplot as plt
-
-def matplotConfig(filename):
-    # xy軸(spine)の移動
-    ax = plt.gca()  # gca stands for 'get current axis'
-    ax.spines['right'].set_color('none')
-    ax.spines['top'].set_color('none')
-    ax.xaxis.set_ticks_position('bottom')
-    ax.spines['bottom'].set_position(('data',0))
-    ax.yaxis.set_ticks_position('left')
-    ax.spines['left'].set_position(('data',0))
-    ax.set_title(filename)
-    ax.set_xlabel('w1')
-    ax.set_ylabel('w0')
-    ax.grid(True) #補助線
-    
-    # グラフとメモリの被りを半透明化
-    for label in ax.get_xticklabels() + ax.get_yticklabels():
-        label.set_fontsize(16)
-        label.set_bbox(dict(facecolor='white', edgecolor='None', alpha=0.65))
-
+import matplotConfig
+        
 def g(X, W):
     v = 0
     for i in range(len(W)):
         v += W[i] * X[i]
     return v
     
-if __name__ == '__main__':
-    
-    # 初期値
-    X = [[1.0, 1.2, 1], [1.0, 0.2, 1], [1.0, -0.2, 1],
-        [1.0, -0.5, 2], [1.0, -1.0, 2], [1.0, -1.5, 2]]
-    W = [-7.0, 2.0] #W0, W1
-    row = 3.6
-    filename = "(" + str(W[1]) + "," + str(W[0]) + "," + str(row) + ").png"
+# 重み空間表示
+def showWeightSpace(X, W, row):
+    plt.figure()
+    filename = "weight(" + str(W[1]) + "," + str(W[0]) + "," + str(row) + ").png"
     
     W_copy = list(W)
     canContinue = True
@@ -79,7 +57,62 @@ if __name__ == '__main__':
         w0 = -X[i][1] * w1
         plt.plot(w1, w0, color, linewidth=1.0, linestyle="-")
     
-    matplotConfig(filename)
+    matplotConfig.forWeight(filename)
+    plt.savefig(filename) #CUIでshowは使えない
     
+# 特徴空間表示
+def showFeatureSpace(X, W , row):
+    plt.figure()
+    filename = "feature(" + str(W[1]) + "," + str(W[0]) + "," + str(row) + ").png"
+    
+    canContinue = True
+    
+    for i in range(len(X)):
+        if X[i][2] == 1:
+            plt.scatter(X[i][1], 0, c = 'red', marker = "o")
+        elif X[i][2] == 2:
+            plt.scatter(X[i][1], 0, c = 'blue', marker = "s")
+    
+    while canContinue:
+        canContinue = False
+        for i in range(len(X)):
+            
+            g_value = g(X[i], W)
+            print(str(X[i]) + ":" + str(W) + ":" + str(g_value))
+            
+            if g_value < 0 and X[i][2] == 1:
+                print("1→2")
+                W[0] = W[0] + row * X[i][0]
+                W[1] = W[1] + row * X[i][1]
+                x = np.linspace(-2, 2)
+                y = W[0] + x * W[1]
+                plt.plot(x, y, c="red", linewidth=1.0, linestyle="-")
+                canContinue = True
+            elif g_value > 0 and X[i][2] == 2:
+                print("2→1")
+                W[0] = W[0] - row * X[i][0]
+                W[1] = W[1] - row * X[i][1]
+                x = np.linspace(-2, 2)
+                y = W[0] + x * W[1]
+                plt.plot(x, y, c="red", linewidth=1.0, linestyle="-")
+                canContinue = True
+    
+    # 学習後識別関数の表示
+    x = np.linspace(-2, 2)
+    y = W[0] + x * W[1]
+    plt.plot(x, y, c="black", linewidth=1.0, linestyle="-")
+    
+    matplotConfig.forFeature(filename)
     plt.savefig(filename) #CUIでshowは使えない
 
+if __name__ == '__main__':
+    
+    # 初期値
+    X = [[1.0, 1.2, 1], [1.0, 0.2, 1], [1.0, -0.2, 1],
+        [1.0, -0.5, 2], [1.0, -1.0, 2], [1.0, -1.5, 2]]
+    W = [-7.0, 2.0] #W0, W1
+    row = 3.6
+    
+    showWeightSpace(X, W, row)
+    W = [-7.0, 2.0] #W0, W1
+    showFeatureSpace(X, W, row)
